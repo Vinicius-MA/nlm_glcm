cbffbca67fdb1f95d8e793f9a16f15549aeb6195
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2021-11-18 14:15:46 -0300

    main, noise_sampling restructured

--- Python/main.py
+++ Python/main.py
@@ -8,25 +8,29 @@ def main():
     indexes = [1, 2, 4, 9, 11, 12, 13, 15, 16, 17, 19, 24]
     sigmaList = [10, 25, 50]
     
-    imageInFolder = 'Banco de Imagens/'
-    imageOutFolder = 'Matlab/Resultados/Imagens Obtidas/'
-    spreadsheetFolder = 'Python/resultados/'
+    imageInFolder = "Banco de Imagens/"
+    imageOutFolder = "Matlab/Resultados/Imagens Obtidas/"
+    slicesOutFolder = "Matlab/Resultados/Slices/"
+    spreadsheetFolder = "Python/resultados/"
 
     filenames = [f'HW_C{x:#03d}_120.jpg' for x in indexes]
 
     for fname in filenames:
     
-            baseImage = BaseImage( f'{fname}', sigmaList, folder=imageInFolder)
-
-            start = time.time()
-            baseImage.generate_noisy_samples(folder = imageOutFolder)
-            diff = time.time() - start
+            baseImage = BaseImage( fname , sigmaList, folder=imageInFolder )
+            baseImage.generate_noisy_samples( folder=imageOutFolder )
             
-            baseImage.generate_nlmLbp_samples(folder = imageOutFolder)
-            baseImage.generate_spreadsheet( folder = spreadsheetFolder)
-
-            print( f'>>>> total {fname} time: {diff:#.01f} s ({diff/60:#.01f} min)')
-
+            # execute NLM-LBP filtering
+            baseImage.generate_nlm_lbp_samples( folder=imageOutFolder )
+            
+            # execute NLM_GLCM filtering
+            baseImage.generate_nlm_glcm_samples( folder=imageOutFolder )
+            
+            # save data to spreadsheet
+            baseImage.generate_spreadsheet( folder=spreadsheetFolder )
+            
+            # generate slices
+            baseImage.generate_slices(outFolder=slicesOutFolder, inFolder=imageOutFolder)
 
 if __name__ == "__main__":
 
--- Python/noise_sampling.py
+++ Python/noise_sampling.py
@@ -6,16 +6,30 @@ import numpy as np
 import openpyxl as xl
 from skimage import io
 
-import nlm_lbp as nlmlbp
 import utils
+from glcm_properties import Props
+from nlm_glcm import nlm_glcm_filter
+from nlm_lbp import nonlocal_means_lbp_original as nlm_lbp_filter
 
-
+ORIGINAL = "original"
 NOISY_OUT_FNAME = "noisy"
+BM3D_OUT_FNAME = "bm3d".upper()
+DA3D_OUT_FNAME = "da3d".upper()
+DDID_OUT_FNAME = "ddid".upper()
+NLDD_OUT_FNAME = "nldd".upper()
+NLM_OUT_FNAME = "nlm".upper()
 NLM_LBP_OUT_FNAME = "nlmlbp"
-NLM_GLCM_OUT_FNAME = "nlm-glcm"
+NLM_GLCM_OUT_FNAME = "nlmglcm"
+
+out_fname = [
+    ORIGINAL,
+    NOISY_OUT_FNAME, BM3D_OUT_FNAME, DA3D_OUT_FNAME, DDID_OUT_FNAME,
+    NLDD_OUT_FNAME, NLM_OUT_FNAME, NLM_LBP_OUT_FNAME, NLM_GLCM_OUT_FNAME
+]
+
 TERMINAL_OUT_CREATED_FILE = "created"
 TERMINAL_OUT_OPENED_FILE = "opened"
-SPREADSHEET_HEADER = [ "Rows", "Noisy", "NLM-LBP" ]
+SPREADSHEET_HEADER = [ "Rows", "Noisy", "NLM-LBP", "NLM-GLCM" ]
 SPREADSHEET_MEDIA = "Média"
 
 class BaseImage:
@@ -40,13 +54,13 @@ class BaseImage:
         self.im_original = _imread( 
                 f'{self.folder}{self.filename}.{self.extension}'
             )
-    
-    """ generate_noisy_samples():
+  
+    def generate_noisy_samples( self, folder="" ) :
+        """ generate_noisy_samples():
             generate both object's noisy images matrix and image file.
             - parameters:
-                folder: name of folder to save images. """    
-    def generate_noisy_samples( self, folder="" ) :
-
+                folder: name of folder to save images. """
+        
         if ( self.im_original is None ) :
             self.open_original()
         
@@ -96,181 +110,34 @@ class BaseImage:
 
                 print( f">>>> {printStr} {folder + fname} - psnr: {psnr:#.03f}" )
 
-    def generate_nlmLbp_samples(self, window_radius = 10, patch_radius = 6, 
+    def generate_nlm_lbp_samples(self, window_radius = 10, patch_radius = 6, 
         lbp_method = 'uniform', lbp_n_points = 16, lbp_radius = 2, folder="" ):
 
-        if ( self.im_original is None ) :
-            self.open_original()
+        self._generate_filter_samples( filterName=NLM_LBP_OUT_FNAME, folder=folder,
+            window_radius=window_radius, patch_radius=patch_radius,
+            lbp_method=lbp_method, lbp_n_points=lbp_n_points, lbp_radius=lbp_radius
+        )
+
+    def generate_nlm_glcm_samples(self, window_radius = 10, patch_radius = 6, 
+        distances = [10], angles = [0], levels=256, props=Props.all(),
+        symmetric=True, folder="" ):
+
+        self._generate_filter_samples( filterName=NLM_GLCM_OUT_FNAME, folder=folder,
+            window_radius=window_radius, patch_radius=patch_radius,
+            glcm_distances=distances, glcm_angles=angles, glcm_levels=levels,
+            glcm_props=props, glcm_symmetric=symmetric
+        )
+
+    def generate_slices( self, outFolder="", origin=(0,0), shape=(100,100),
+         startStr="detail", sample=1, inFolder=""
+        ):
+
+        for filterName in out_fname:
+            
+            self._generate_filter_slices(outFolder=outFolder, filterName=filterName,
+            origin=origin, shape=shape, startStr=startStr, sample=sample, inFolder=inFolder
+        )
         
-        # Generate Matrix of NLM-LBP Processed Images
-        self.nlmLbpImages = np.zeros(
-            [ len( self.sigmaList ), self.samples,
-            self.im_original.shape[0], self.im_original.shape[1] ],
-            dtype=np.uint8
-        )
-
-        # Generate NLM-LBP Processed PSNR matrix
-        self.nlmLbpPsnr = np.zeros(
-            [ len(self.sigmaList), self.samples ], dtype=np.float64
-        )
-
-        sigma_time = 0
-
-        # Generate NLM LBP Images
-        for ( k, sigma ) in enumerate( self.sigmaList ) :
-
-            sample_time = 0
-
-            for i in range( self.samples ):
-
-                # current processed image file name
-                fname = get_nlm_lbp_sample_filename( f'{self.filename}.{self.extension}', sigma, i+1 )
-                fullFilePath = f'{folder}{fname}'
-
-                if( not( exists( fullFilePath) ) ):
-
-                    printStr = TERMINAL_OUT_CREATED_FILE
-
-                    print( f'>>> starting NLM-LBP process for {fullFilePath}')
-                    
-                    # recover Noisy Image
-                    im_noisy = self.noisyImages[k, i, :, :]
-
-                    start_time = time.time()
-                    
-                    im_proc =  (
-                        nlmlbp.nonlocal_means_lbp_original( im_noisy,
-                            window_radius, patch_radius, sigma, lbp_method,
-                            lbp_n_points, lbp_radius
-                        )
-                    )
-
-                    diff = time.time() - start_time
-
-                    # save to file
-                    io.imsave( fullFilePath, im_proc )
-                    
-                else:
-
-                    printStr = TERMINAL_OUT_OPENED_FILE
-
-                    im_proc = io.imread( fullFilePath )
-                        
-                # save to Class object
-                self.nlmLbpImages[k, i, :, :] = im_proc 
-
-                # calculate psnr
-                psnr = utils.calculate_psnr( self.im_original, im_proc )
-                self.nlmLbpPsnr[k, i] = psnr
-
-                if 'diff' in locals():
-                    
-                    sample_time += diff
-                    sigma_time += diff
-
-                    print( f">>>> {printStr} {folder + fname} - psnr: {psnr:#.03f}" + 
-                        f" - time: {diff:#.01f} s ({diff/60:#.01f} min)"
-                    )
-
-                print( f">>>> {printStr} {folder + fname} - psnr: {psnr:#.03f}" )
-
-
-            print( f'>>>>\ttotal sample time:  {sample_time:#.01f} s' +
-                f' ({sample_time/60:#.01f} min)'
-            )
-
-        print( f'>>>>\ttotal sigma time:  {sigma_time:#.01f} s' +
-                f' ({sigma_time/60:#.01f} min)'
-            )
-
-    def generate_nlmGlcm_samples(self, window_radius = 10, patch_radius = 6, 
-        glcm_distances = [1], glcm_angles = [0], glcm_levels=256,
-        glcm_symmetric=False, glcm_normed=True, folder="" ):
-
-        if ( self.im_original is None ) :
-            self.open_original()
-        
-        # Generate Matrix of NLM-GLCM Processed Images
-        self.nlmGlcmImages = np.zeros(
-            [ len( self.sigmaList ), self.samples,
-            self.im_original.shape[0], self.im_original.shape[1] ],
-            dtype=np.uint8
-        )
-
-        # Generate NLM-GLCM Processed PSNR matrix
-        self.nlmGlcmPsnr = np.zeros(
-            [ len(self.sigmaList), self.samples ], dtype=np.float64
-        )
-
-        sigma_time = 0
-
-        # Generate NLM GLCM Images
-        for ( k, sigma ) in enumerate( self.sigmaList ) :
-
-            sample_time = 0
-
-            for i in range( self.samples ):
-
-                # current processed image file name
-                fname = get_nlm_glcm_sample_filename( f'{self.filename}.{self.extension}', sigma, i+1 )
-                fullFilePath = f'{folder}{fname}'
-
-                if( not( exists( fullFilePath) ) ):
-
-                    printStr = TERMINAL_OUT_CREATED_FILE
-
-                    print( f'>>> starting NLM-GLCM process for {fullFilePath}')
-                    
-                    # recover Noisy Image
-                    im_noisy = self.noisyImages[k, i, :, :]
-
-                    start_time = time.time()
-                    
-                    im_proc =  (
-                        nlmlbp.nonlocal_means_lbp_original( im_noisy,
-                            window_radius, patch_radius, sigma, lbp_method,
-                            lbp_n_points, lbp_radius
-                        )
-                    )
-
-                    diff = time.time() - start_time
-
-                    # save to file
-                    io.imsave( fullFilePath, im_proc )
-                    
-                else:
-
-                    printStr = TERMINAL_OUT_OPENED_FILE
-
-                    im_proc = io.imread( fullFilePath )
-                        
-                # save to Class object
-                self.nlmLbpImages[k, i, :, :] = im_proc 
-
-                # calculate psnr
-                psnr = utils.calculate_psnr( self.im_original, im_proc )
-                self.nlmLbpPsnr[k, i] = psnr
-
-                if 'diff' in locals():
-                    
-                    sample_time += diff
-                    sigma_time += diff
-
-                    print( f">>>> {printStr} {folder + fname} - psnr: {psnr:#.03f}" + 
-                        f" - time: {diff:#.01f} s ({diff/60:#.01f} min)"
-                    )
-
-                print( f">>>> {printStr} {folder + fname} - psnr: {psnr:#.03f}" )
-
-
-            print( f'>>>>\ttotal sample time:  {sample_time:#.01f} s' +
-                f' ({sample_time/60:#.01f} min)'
-            )
-
-        print( f'>>>>\ttotal sigma time:  {sigma_time:#.01f} s' +
-                f' ({sigma_time/60:#.01f} min)'
-            )
-
     def generate_spreadsheet(self, fname=None, folder=""):
 
         print( f'>>>> generate_spreadsheet:')
@@ -291,7 +158,12 @@ class BaseImage:
 
             for row in range(self.samples):
                 
-                currRow = ( row+1, self.noisyPsnr[k, row], self.nlmLbpPsnr[k, row] )
+                currRow = ( 
+                    row+1,
+                    self.noisyPsnr[k, row],
+                    self.nlmLbpPsnr[k, row],
+                    self.nlmGlcmPsnr[k, row]
+                )
                 
                 currSheet.append( currRow )
                 print( f"\tadded to spreadsheet:\t{currRow}" )
@@ -300,7 +172,8 @@ class BaseImage:
             currRow = (
                     SPREADSHEET_MEDIA,
                     statistics.mean( self.noisyPsnr[k, :] ),
-                    statistics.mean( self.nlmLbpPsnr[k, :] )
+                    statistics.mean( self.nlmLbpPsnr[k, :] ),
+                    statistics.mean( self.nlmGlcmPsnr[k, :] )
                 )
             
             print( f"\tadded to spreadsheet:\t{currRow}" )
@@ -315,17 +188,205 @@ class BaseImage:
     def set_filename(self, newfilename):
         
         self.filename = newfilename
+    
+    def _generate_filter_slices(self, outFolder="", filterName=NLM_GLCM_OUT_FNAME, 
+         origin=(0,0), shape=(100,100), startStr="detail", sample=0, inFolder=""
+        ):
+        """ Execute after filtering is done """
+        
+        if ( self.im_original is None ):
+            self.open_original()
+
+        if ( filterName == ORIGINAL):
+            images = np.zeros(
+                [1, 1, self.im_original.shape[0], self.im_original.shape[1]],
+                dtype=np.uint8
+            )
+            images[ 0, 0, :, :] = self.im_original
+
+            sample=0
+
+        elif ( filterName == NLM_GLCM_OUT_FNAME ):
+            images = self.nlmGlcmImages
+        elif ( filterName == NLM_LBP_OUT_FNAME ):
+            images = self.nlmLbpImages
+        elif ( filterName == NOISY_OUT_FNAME ):
+            images = self.noisyImages
+        else:
+            images = np.zeros( [len(self.sigmaList), 1,
+                self.im_original.shape[0], self.im_original.shape[1] ], dtype=np.uint8
+            )
+
+            for (k, sigma ) in enumerate( self.sigmaList ):
+                
+                fullFilePath = inFolder + _get_sample_filename(
+                    f"{self.filename}.{self.extension}", sigma, sample, filterName
+                )
+
+                images[ k, 0, :, :] = _imread( fullFilePath )
+
+                print( f">>>> opened: {fullFilePath}" )
+
+            sample=0
+
+        # define slices dimensions
+        ( y0, x0 ) = origin
+        ( dy, dx ) = shape
+
+        # generate matrix of slices
+        if ( filterName==ORIGINAL ):
+            slices = np.zeros([ 1, dy, dx ], dtype=np.uint8 )
+        else:
+            slices = np.zeros([ len(self.sigmaList), dy, dx ], dtype=np.uint8 )
+
+        for ( k, sigma ) in enumerate( self.sigmaList ):
+            
+            if ( filterName == ORIGINAL ):
+                slices[ 0, :, :] = images[0, 0, y0:y0+dy, x0 : x0+dx]
+            else:
+                slices[ k, :, : ] = images[ k, sample, y0: y0+dy , x0 : x0+dx ]
+            
+            fname = _get_slice_filename(
+                f"{self.filename}.{self.extension}", sigma, startStr, filterName
+            )
+
+            # save image to file
+            if (filterName == ORIGINAL ):
+                io.imsave( outFolder+fname, slices[0,:,:])
+            else:
+                io.imsave( outFolder+fname, slices[k,:,:])
+
+            print( f">>>> slice saved: {outFolder + fname} shape:( {slices[0,:,:].shape[0]}, {slices[0,:,:].shape[1]} )")
+
+    def _generate_filter_samples(self, folder="", window_radius = 10, patch_radius = 6, 
+         filterName=NLM_GLCM_OUT_FNAME,
+         lbp_method='uniform', lbp_n_points=16, lbp_radius=2,
+         glcm_distances=[10], glcm_angles=[0], glcm_levels=256, glcm_props=Props.all(),
+         glcm_symmetric=True
+        ):
+
+        if ( self.im_original is None ) :
+            self.open_original()
+
+        # Generate Matrix of Processed Images
+        images = np.zeros( [ len( self.sigmaList ), self.samples, 
+            self.im_original.shape[0], self.im_original.shape[1] ],
+            dtype=np.uint8
+        )
+
+        # Generate Processed PSNR matrix
+        psnrs = np.zeros(
+            [ len(self.sigmaList), self.samples ], dtype=np.float64
+        )
+
+        sigma_time = 0
+
+        # Generate Processed Images
+        for ( k, sigma ) in enumerate( self.sigmaList ) :
+
+            sample_time = 0
+
+            for i in range( self.samples ):
+
+                # current processed image file name
+                fname = _get_sample_filename(f'{self.filename}.{self.extension}', sigma, i+1, filterName)
+                fullFilePath = f'{folder}{fname}'
+
+                if( not( exists( fullFilePath) ) ):
+
+                    printStr = TERMINAL_OUT_CREATED_FILE
+                    
+                    # recover Noisy Image
+                    im_noisy = self.noisyImages[k, i, :, :]
+
+                    start_time = time.time()
+                    
+                    if ( filterName == NLM_GLCM_OUT_FNAME):
+                        im_proc =  (
+                            nlm_glcm_filter(im_noisy, window_radius, patch_radius, sigma,
+                                glcm_distances, glcm_angles, glcm_levels,
+                                glcm_props, glcm_symmetric )
+                        )
+                    elif ( filterName == NLM_LBP_OUT_FNAME ):
+                        im_proc =  (
+                            nlm_lbp_filter( im_noisy,
+                                window_radius, patch_radius, sigma, lbp_method,
+                                lbp_n_points, lbp_radius )
+                        )
+
+                    diff = time.time() - start_time
+
+                    # save to file
+                    io.imsave( fullFilePath, im_proc )
+                    
+                else:
+
+                    printStr = TERMINAL_OUT_OPENED_FILE
+
+                    im_proc = io.imread( fullFilePath )
+                        
+                # save to Class object
+                images[k, i, :, :] = im_proc 
+
+                # calculate psnr
+                psnr = utils.calculate_psnr( self.im_original, im_proc )
+                psnrs[k, i] = psnr
+
+                if 'diff' in locals():
+                    
+                    sample_time += diff
+                    sigma_time += diff
+
+                    print( f">>>> {printStr} {folder + fname} - psnr: {psnr}" + 
+                        f" - time: { int( diff // 60 ):#02d}:{ int( diff % 60 ):#02d}"
+                    )
+                else:
+                    print( f">>>> {printStr} {folder + fname} - psnr: {psnr}" )
+
+                if (filterName == NLM_GLCM_OUT_FNAME ):
+                    self.nlmGlcmImages = images
+                    self.nlmGlcmPsnr = psnrs
+
+                elif ( filterName == NLM_LBP_OUT_FNAME ):
+                    self.nlmLbpImages = images
+                    self.nlmLbpPsnr = psnrs
+
+
+            print( f">>>>\ttotal sample time: " +
+                f"{ int( sample_time//60 ):#02d}:{ int( sample_time % 60 ):#02d}"
+            )
+
+        print( f">>>>\t\ttotal sigma time: " +
+            f"{ int( sigma_time // 60 ):#02d}:{ int( sigma_time % 60 ):#02d}"
+        )
 
 def get_noisy_sample_filename ( filename, sigma, sample ):
-        return _get_sample_filename(filename, sigma, sample, NOISY_OUT_FNAME )
+        return _get_sample_filename( filename, sigma, sample, NOISY_OUT_FNAME )
 
 def get_nlm_lbp_sample_filename( filename, sigma, sample ):
-    return _get_sample_filename(filename, sigma, sample, NLM_LBP_OUT_FNAME )
+    return _get_sample_filename( filename, sigma, sample, NLM_LBP_OUT_FNAME )
 
 def get_nlm_glcm_sample_filename( filename, sigma, sample ):
     return _get_sample_filename(filename, sigma, sample, NLM_GLCM_OUT_FNAME)
 
-""" _get_sample_filename()
+def get_noisy_slice_filename( filename, sigma, startStr="detail" ):
+    return _get_slice_filename( filename, sigma, startStr, NOISY_OUT_FNAME )
+
+def get_nlm_lbp_slice_filename( filename, sigma, startStr="detail" ):    
+    return _get_slice_filename( filename, sigma, startStr, NLM_LBP_OUT_FNAME )
+
+def get_nlm_glcm_slice_filename( filename, sigma, startStr="detail"):
+    return _get_slice_filename(filename, sigma, startStr, NLM_GLCM_OUT_FNAME )
+
+def _get_slice_filename( filename, sigma, startStr="detail", endStr="" ):
+    """  """
+
+    return f"{startStr}_" + filename.replace(".",
+        f"_sigma{sigma:#03d}_{endStr}."
+    )
+
+def _get_sample_filename(filename, sigma, sample, endStr="" ):
+    """ _get_sample_filename()
         Creates a new filename string adding the input information onto the
          filename string.
         - parameters:
@@ -334,14 +395,14 @@ def get_nlm_glcm_sample_filename( filename, sigma, sample ):
             sample: index of the sampled file that will receive this function's
              output string;
             endStr: final part of the output that indicates what type of image
-             is that (e.g 'noisy', 'nlmlnp'). """
-def _get_sample_filename(filename, sigma, sample, endStr="" ):
+             is that (e.g 'noisy', 'nlmlbp')."""
 
-    noisyFilename = filename.replace(".",
+    if endStr == ORIGINAL:
+        return filename
+    
+    return filename.replace(".",
             f"_sigma{sigma:#03d}_{sample:#02d}_{endStr}."
         )
-
-    return noisyFilename
 
 def _imread( fullFilePath ):
 
