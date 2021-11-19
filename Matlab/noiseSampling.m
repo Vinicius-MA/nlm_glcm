59ba59c85c37c322ee46795be644896e268543e5
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2021-11-18 22:42:56 -0300

    MATLAB: noiseSampling, psnrComparacao updated

--- Matlab/noiseSampling.m
+++ Matlab/noiseSampling.m
@@ -2,45 +2,81 @@
 %               COMPARAÇÃO DE MÉTODOS BM3D, DDID, NLDD          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Ruído utilizado: Gaussiano, média = 0, variância = sigma^2
-function [psnr_result]=noiseSampling(Y,sigma,samples,save_foldername,filename)
+function [ psnr_result ] = noiseSampling( Y, sigma, samples, input_folder, save_folder, filename)
 
     
-n_methods = 6;
+    n_methods = 6;
 
     extension =         '.jpg';
-    noisy_subtitle =    '_Ruidosa';
-    ddid_subtitle =     '_DDID';
-    bm3d_subtitle =     '_BM3D';
-    nldd_subtitle =     '_NLDD';
-    da3d_subtitle =     '_DA3D';
-    nlm_subtitle =      '_NLM';
+    noisy_subtitle =    '_noisy';
+    ddid_subtitle =     '_ddid';
+    bm3d_subtitle =     '_bm3d';
+    nldd_subtitle =     '_nldd';
+    da3d_subtitle =     '_da3d';
+    nlm_subtitle =      '_nlm_matlab';
 
     psnr_result = double(zeros(samples+1, n_methods));
+    
+    % sigma in [0, 255] ; v in [0, 1]
     v = (sigma/(2.^8-1)).^2;
     
     for i =1:samples
     
-        upper_title = [filename,'_sigma',num2str(sigma,'%03d'),'_sample',num2str(i,'%02d')];        
+        upper_title = [filename,'_sigma',num2str(sigma,'%03d'),'_',num2str(i,'%02d')];        
+        
+        fprintf( "%s\r\n", upper_title ) ;
 
-        %   Obter Imagem ruidosa
+        %   Noisy -- [0, 1]
+        if isfile( [ input_folder, upper_title, noisy_subtitle, extension ] )
+            Yn = mat2gray( imread( [ input_folder, upper_title, noisy_subtitle, extension ] ), [0, 255] );
+        else
             Yn = imnoise(Y, 'gaussian', 0, v);
+        end
+        
+        fprintf("\tddid\r\n");
 
-        %   Filtro DDID
+        %   DDID
+        if isfile( [ save_folder, upper_title, ddid_subtitle, extension ] )
+            Yddid = mat2gray( imread( [ save_folder, upper_title, ddid_subtitle, extension ] ), [0, 255] );
+        else
             Yddid = DDID( Yn, v);
-
-        %   Filtro BM3D
-            [~, Ybm3d] = BM3D(Y, Yn, sigma,'np',0);
-            %Ybm3d = im2uint8(Ybm3d);
+        end
             
-        %   Filtro NLDD
+
+        %   BM3D
+        if isfile( [ save_folder, upper_title, bm3d_subtitle, extension ] )
+            Ybm3d = mat2gray( imread( [ save_folder, upper_title, bm3d_subtitle, extension ] ), [0, 255] );
+        else
+            % must provide sigma even with Yn in [0,1]
+            [~, Ybm3d] = BM3D( 1, Yn, sigma );
+        end
+            
+            
+        %   NLDD
+        if isfile( [ save_folder, upper_title, nldd_subtitle, extension ] )
+            Ynldd = mat2gray( imread( [ save_folder, upper_title, nldd_subtitle, extension ] ), [0, 255] );
+        else
+            %   passing standar deviation as argument
             Ynldd = NLDD(Yn, v^0.5, 1);
+        end
+            
             
         %   DA3D + NL-Bayes
+        if isfile( [ save_folder, upper_title, da3d_subtitle, extension ] )
+            Yda3d = mat2gray( imread( [ save_folder, upper_title, da3d_subtitle, extension ] ), [0, 255] );
+        else
+            %   passing standar deviation as argument
             Ybayes = NlBayesDenoiser(Yn, v^0.5, 1);
-            Yda3d = DDIDstep( Ybayes, Yn, v, 15, 7, 0.7, 0.8);   %   os últimos 4 parâmetros, não sei o que é
+            Yda3d = DDIDstep( Ybayes, Yn, v, 15, 7, 0.7, 0.8);   %   os últimos 4 parâmetros, não sei o que são 
+        end
         
         %   Filtro NLM
-            Ynlm = nlm( Y, Yn, 10, 3, 10*v);
+        if isfile( [ save_folder, upper_title, nlm_subtitle, extension ] )
+            Ynlm = mat2gray( imread( [ save_folder, upper_title, nlm_subtitle, extension ] ), [0, 255] );
+        else
+            Ynlm = nlm( Y, Yn, 10, 3, v);
+        end
+            
 
         %   PSNRs (noisy, DDID, BM3D)
             % noisy, DDID, BM3D, NLDD, DA3D, NLM
@@ -57,21 +93,21 @@ n_methods = 6;
 
          %  SALVAR IMAGENS
             %   Imagem Ruidosa
-            imwrite(Yn,[save_foldername,upper_title,noisy_subtitle,extension]);
+            imwrite(Yn,[save_folder,upper_title,noisy_subtitle,extension]);
 
             %   Imagem DDID
-            imwrite(Yddid,[save_foldername,upper_title,ddid_subtitle,extension]);
+            imwrite(Yddid,[save_folder,upper_title,ddid_subtitle,extension]);
 
             %   Imagem BM3D 
-            imwrite(Ybm3d,[save_foldername,upper_title,bm3d_subtitle,extension]);
+            imwrite(Ybm3d,[save_folder,upper_title,bm3d_subtitle,extension]);
             
             %   Imagem NLDD
-            imwrite(Ynldd,[save_foldername,upper_title,nldd_subtitle,extension]);
+            imwrite(Ynldd,[save_folder,upper_title,nldd_subtitle,extension]);
             
             %   Imagem DA3D + NL-Bayes
-            imwrite(Yda3d,[save_foldername,upper_title,da3d_subtitle,extension]);
+            imwrite(Yda3d,[save_folder,upper_title,da3d_subtitle,extension]);
             
             %   Imagem NLM
-            imwrite(Ynlm, [save_foldername, upper_title, nlm_subtitle, extension]);
+            imwrite(Ynlm, [save_folder, upper_title, nlm_subtitle, extension]);
     end
 end
--- Matlab/psnr_comparacao.m
+++ Matlab/psnr_comparacao.m
@@ -2,12 +2,10 @@ clear all
 close all
 clc
 
-base_folder = 'C:\Users\vinic\Documents\TCC\Banco de Imagens\Hardware original\';
-save_folder = 'D:\Documentos\Graduação\TCC - Resultados\Imagens Obtidas\';
-table_folder = 'D:\Documentos\Graduação\TCC - Resultados\';
-
-%save_folder = 'D:\Documentos\Graduação\TCC - Testes\';
-%table_folder = 'D:\Documentos\Graduação\TCC - Testes\';
+base_folder = 'Banco de Imagens\';
+input_folder = 'NovoNoisy\';
+save_folder = 'NovoObtidas\';
+table_folder = 'NovoResultados\';
 
 samples = 10;                         %   amostras por imagem carregada
 im_code = [1, 2, 4, 9, 11, 12, 13, 15, 16, 17, 19, 24];
@@ -29,18 +27,16 @@ end
 
 for k=1:size(filename,2)
 
+    % Y in [0, 1]
     Y = mat2gray( rgb2gray( imread( [base_folder,filename{k},extension]) ), [0, 255] );
-    %Y = imresize(Y(:,:,1), 1.);
-
-    %imwrite(Y,[save_folder,filename{i},original_subtitle,extension]);
 
     for i=1:size(sigma,2)
-        psnr_result = noiseSampling(Y,sigma(i),samples,save_folder,filename{k});
+        psnr_result = noiseSampling( Y,sigma(i),samples,input_folder, save_folder, filename{k} ) ;
 
         T = table(psnr_result(:,1),psnr_result(:,2),psnr_result(:,3), psnr_result(:,4), psnr_result(:,5), psnr_result(:,6),...
             'VariableNames',{'Noisy','DDID','BM3D','NLDD', 'DA3D', 'NLM'},'RowNames',rownames);
 
-        writetable(T,[table_folder,filename{k},'.xlsx'],'Sheet',['sigma_',num2str(sigma(i),'%03d')],...
+        writetable(T,[table_folder,filename{k},'_matlab.xlsx'],'Sheet',['sigma_',num2str(sigma(i),'%03d')],...
             'Range','A1','WriteRowNames',true);
     end
 end
